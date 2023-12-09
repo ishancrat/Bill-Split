e74408aeb23cbf3a8e41fa6a2e9995e4800e4723
Ishan Bajpai <82746770+ishancrat@users.noreply.github.com>
2023-12-09 13:44:50 +0530

    Update billSplit.py
    Resolved a bug. Spreadsheet creation functionality added.

--- billSplit.py
+++ billSplit.py
@@ -1,20 +1,46 @@
 import numpy as np
+import pandas as pd
+from datetime import datetime 
+
+tripWasToday = input("was the trip scheduled today? : ")
+if (tripWasToday.upper() == "Y"):
+    date = datetime.now()
+    date = date.strftime("%d-%m-%Y")
+else:
+    date = input("enter trip's date in format DD-MM-YYYY (hyphens included): ")
+header = []
+header.append(["Expense"])
 
 nameStr=input("Enter names (space separated): ")
 print("--------------------------------------------")
 names=[i for i in nameStr.split()]
+
+for i in names:
+    header[0].append(i)
+    
+rowPtr = 1
+
 numPpl=len(names)
 score=np.zeros((1,numPpl))
+
+
 while(True):
+    templst = []
+    temp=[]
+    moreThanOnePayer = False
     for i in range(numPpl):
         print("[",i,"] ",names[i],end="   ")
+    expenseName = input("Expense Title: ")
+    templst.append(expenseName)
+    
     expense=int(input("enter total expense for item: "))
-    temp=[]
+    
     temp=[int(i) for i in input("space separated ID of payers: ").split()]
      
     if (len(temp)>1):
+        moreThanOnePayer = True
         tempPaid=[]
-        tempPaid=[int(i) for i in input("amt paid by the above respectively: ").split()]
+        tempPaid=[float(i) for i in input("amt paid by the above respectively: ").split()]
 
     
     for i in range(len(temp)):
@@ -33,31 +59,50 @@ while(True):
             perHead=expense/numPpl
             for i in range(numPpl):
                 score[0][i]-=perHead
+                templst.append(perHead)
+                
         else:
             personWiseExpense=[]
-            personWiseExpense=[int(i) for i in input("person-wise expense in order of ID: ").split()]
+            personWiseExpense=[float(i) for i in input("person-wise expense in order of ID: ").split()]
                 
             for i in range(numPpl):
                 score[0][i]-=personWiseExpense[i]
+                templst.append(personWiseExpense[i])
     else:
         if (equallySplit=="y"):
             userID=[]
             userID=[int(i) for i in input("enter ID of consumers (space separated): ").split()]
-                
+            numWhoDidNotConsume = len(userID)
+            blankLst = [0 for i in range(numPpl)]
+            templst = templst + blankLst
+            
             for i in range(len(userID)):
-                temp=userID[i]
-                score[0][temp]-=expense/(len(userID))
+                tempp=userID[i]
+                score[0][tempp]-=expense/(len(userID))
+                templst[userID[i]+1] = (expense/len(userID))
         else:
 
             userID=[]
             userID=[int(i) for i in input("enter ID of consumers (space separated): ").split()]
-            
+            blankLst = [0 for i in range(numPpl)]
+            templst = templst + blankLst
             userExpense=[]
-            userExpense=[int(i) for i in input("enter amt respectively consumed (space separated): ").split()]
+            userExpense=[float(i) for i in input("enter amt respectively consumed (space separated): ").split()]
             
             for i in range(len(userID)):
-                temp=userID[i]
-                score[0][temp]-=userExpense[i]
+                tempp=userID[i]
+                score[0][tempp]-=userExpense[i]
+                templst[userID[i]+1] = (userExpense[i])
+                
+    if (moreThanOnePayer):
+        for i in range(len(temp)):
+            templst.append(tempPaid[i])
+            templst.append(names[temp[i]])
+    else:
+        templst.append(expense)
+        templst.append(names[temp[0]])
+        
+    header.append(templst)
     flag=input("more expenses?: ")
     if (flag=="y"):
         print()
@@ -65,18 +110,24 @@ while(True):
     else:
         break
 
+header.append([])     
 print()        
 print("-------------------------------------------------")
 print()
 print("------------NET BALANCE----------------")
 for i in range(numPpl):
+    temp=[]
     print(i,end="  ")
     print(names[i],end="   ")
+    temp.append(names[i])
     print(score[0][i])
+    temp.append(score[0][i])
+    header.append(temp)
 print()
 print("-----------------VERDICT---------------")
 print()
 
+header.append([])
 
 ID=[int(i) for i in range(numPpl)]
 
@@ -107,11 +158,14 @@ for i in range(numPpl):
 j=0
 i=0 
 
+
 while(True):
     if i==len(posArr):
         break
     if j==len(negArr):
         break
+    temp=[]
+    
     
     if (abs(negArr[j])<posArr[i]):
         amt=abs(negArr[j])
@@ -119,14 +173,24 @@ while(True):
         posArr[i]-=amt
         if (amt!=0):
             print(amt," paid by ",names[IDneg[j]]," to ",names[IDpos[i]])
+            temp.append(names[IDneg[j]])
+            temp.append("to")
+            temp.append(names[IDpos[i]])
+            temp.append(amt)
+        header.append(temp)  
         j+=1
-        i-=1
+#         i-=1
     elif (abs(negArr[j])==posArr[i]):
         amt=abs(negArr[j])
         negArr[j]=0
         posArr[i]-=amt
         if (amt!=0):
             print(amt," paid by ",names[IDneg[j]]," to ",names[IDpos[i]])
+            temp.append(names[IDneg[j]])
+            temp.append("to")
+            temp.append(names[IDpos[i]])
+            temp.append(amt)
+        header.append(temp)
         j+=1
         i+=1
     else:
@@ -134,5 +198,11 @@ while(True):
         posArr[i]=0
         if (amt!=0):
             print(amt," paid by ",names[IDneg[j]]," to ",names[IDpos[i]])
+            temp.append(names[IDneg[j]])
+            temp.append("to")
+            temp.append(names[IDpos[i]])
+            temp.append(amt)
+        header.append(temp)
         negArr[j]+=amt
-        i+=1          
+        i+=1
+        
